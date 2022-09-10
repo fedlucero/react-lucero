8a0a8081de6b2cf906a164b1e1d143555156e437
Federico Lucero <cfede.lucero@gmail.com>
2022-09-10 16:23:51 -0300

    prueba skeleton

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Item from '../Item/Item';
 import './itemList.css'
+import MyLoader from '../Loader/Loader';
 
 const ItemList = ({ items }) => {
+    // return (
+    //     <div className='containerArc' >
+    //         {items.map((item) => (
+    //             <Item key={item.id} item={item} id={item.id} type={item.type} name={item.name} price={item.price} stock={item.stock} img={item.img} description={item.description} estilo={item.estilo} />
+    //         ))}
+    //     </div>
+    // );
     return (
-        <div className='containerArc' >
-            {items.map((item) => (
+            <div className='containerArc' >
+                    {
+            items.length > 0
+            ? items.map((item) => (
                 <Item key={item.id} item={item} id={item.id} type={item.type} name={item.name} price={item.price} stock={item.stock} img={item.img} description={item.description} estilo={item.estilo} />
-            ))}
-        </div>
+            ))
+            : <MyLoader/>
+        }
+            </div>
     );
 };
 export default ItemList;
