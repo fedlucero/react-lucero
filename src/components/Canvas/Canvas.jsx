2a74658e2886d9a021097976d6280734a4ef0f0b
Federico Lucero <cfede.lucero@gmail.com>
2022-10-05 02:52:56 -0300

    detalles

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -7,7 +7,6 @@ const Canvas = props => {
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx= canvas.getContext('2d')
-    //Our first draw
     let value = 0
 const color = ['#76A7F4','#6B8CE6','#6071D7','#8671E1','#AB71EB','#B188EF','#B69FF2','#BCB6F6','#C1CDF9','#20331A','#33512A','#446C37','#558745','#66A253','#7CB36B','#94C186','#ABCEA1','#C3DCBC','#8C2F39','#9F3541','#B23A48','#BE4E59','#CA626A','#D6767B','#E18A8B','#EFA8A1','#F6B7AC']
 
