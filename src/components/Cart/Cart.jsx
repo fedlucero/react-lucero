bcbae6d770ef6c1a685391c5d734504843141024
Federico Lucero <cfede.lucero@gmail.com>
2022-10-11 14:16:58 -0300

    form

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,72 +1,20 @@
-
 import React from 'react';
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import {CartContext} from '../Context/CartContext'
 import ItemCart from '../ItemCart/ItemCart';
-import { doc, setDoc, collection, updateDoc } from "firebase/firestore";
-import { increment, serverTimestamp } from "firebase/firestore";
-import { db } from '../../utils/firebaseConfig';
 import './cart.css'
-import Swal from 'sweetalert2';
 
 
-// function Cart(){
-//     // const { items, totalPrice, clearCart } = CartContext();
-//     const ctx = CartContext();
 
-const Cart = () => {
-    const ctx = useContext(CartContext);
-
-    const createOrder = async () => {
-        let itemsForDB = ctx.cart.map(item => ({
-            id:item.id,
-            title:item.name,
-            price:item.price,
-            quantity:item.total
-        }))
-        let order = {
-            buyer: {
-                name:'nombre',
-                mail:'mail',
-                phone:'tel'
-            },
-            items: itemsForDB,
-            date: serverTimestamp(),
-            total:ctx.totalPrice()      
-        }
-        // console.log(order)
-        const newOrderRef = doc( collection(db, "orders") )
-        await setDoc( newOrderRef, order)
-
-
-        Swal.fire(
-            'Tu orden fue creada!',
-            "Este es tu ID de orden: "+ newOrderRef.id
-        )
-       
-        ctx.clearCart()
-    
-        
-        itemsForDB.map(async (item) => {
-            const itemRef = doc(db, "products",item.id);
-            await updateDoc (itemRef,{
-                stock: increment(-parseInt(item.quantity))
-            })
-        })
-
-       
-
-        
-        
-    }
-        
+function Cart(){
+    const { cart, totalPrice, clearCart } = CartContext();
+   
   
     return (
         
             <>
                 
-                {ctx.cart.length === 0 ? (  
+                {cart.length === 0 ? (  
                 <div className="cartVacio">
                         <p className="mensajeCarritoVacio">El carrito esta vacio</p>
                         <Link to={'/'}><div className={`btn volver coloresFondo`}>Volver a la tienda</div></Link>
@@ -76,7 +24,7 @@ const Cart = () => {
                 <div className="cartConArc"> 
                     <div className="cartItem">            
                         {
-                            ctx.cart.map(item => <ItemCart key={item.id} item={item} />)
+                            cart.map(item => <ItemCart key={item.id} item={item} />)
                         }
                     </div>
                     <div className='vacio'></div>
@@ -84,15 +32,18 @@ const Cart = () => {
                     <div className='cartContext'>
                         
                         <p className="cartTotal">
-                                Total : $ {ctx.totalPrice()}
+                                Total : $ {totalPrice()}
                         </p>
-                       <div className={`btn coloresFondo`} onClick={ ctx.clearCart  }>Vaciar carrito</div>
+                       <div className={`btn coloresFondo`} onClick={ clearCart  }>Vaciar carrito</div>
                         <div className={`btn coloresFondo`}>
                              <Link style={{color:'white'}} to="/">Seguir comprando</Link>
                         </div>
-                        <div className={`btn coloresFondo`} onClick={createOrder}> 
+                        <div className={`btn coloresFondo`}> 
+                            <Link to="/Form">
                             <div style={{color:'white'}}  >Comprar</div>
+                            </Link>
                         </div>
+                            
                                 
                         
                     </div>
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,184 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner'
+import './Form.css';
+import {useCartContext} from '../Context/CartContext'
+import { addDoc, collection, getFirestore, doc, updateDoc } from 'firebase/firestore';
+
+function Form(){
+    const {cart, totalPrice, clearCart } = useCartContext();
+    const [orderId, setOrderId] = useState(false);
+    const [loading, setLoading] = useState(false)
+    const [buyer, setBuyer] = useState({
+        Nombre:'',
+        Domicilio:'',
+        Localidad:'',
+        Telefono:'',
+        Email:'',
+        ConfirmarEmail:''
+    });
+
+    const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+	const telefonoRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{1,6}$/im
+
+    const { Nombre, Domicilio, Localidad, Telefono, Email, ConfirmarEmail } = buyer
+
+    const handleInputChange = (e) => {
+
+        const { name, value } = e.target
+        setBuyer(({
+            ...buyer,
+            [name]:value
+        }))
+    }
+
+    function updateStocks(cart) {
+        cart.forEach((cart) => {
+            const db = getFirestore();
+            const stockDoc = doc(db, 'data', cart.id);
+            updateDoc(stockDoc, { stock: cart.stock - cart.quantity });
+            
+        });
+    }
+    
+    const addOrder = () => {
+
+        setLoading(true)
+
+        const order = {
+            
+            buyer,
+            items: cart.map((product) => ({
+                id: product.id,
+                title: product.nombre,
+                price: product.precio,
+                quantity: product.quantity,
+            })),
+            date: Date(),
+            total: totalPrice()
+        }
+
+        const db = getFirestore();
+        const ordersCollection = collection(db, "orders");
+        addDoc(ordersCollection, order).then(({ id }) => {
+            setOrderId(id)
+            updateStocks(cart)
+            setLoading(false)
+        });
+        
+    };
+
+    const handleSubmit  = (e) => {
+        e.preventDefault()
+        addOrder();
+        clearCart();
+    }
+    
+    
+    if( orderId != "" ) {
+        return (
+            <div className="tiketContenedor">
+                <div className="tiket">
+                    <h2 className="tiketTitulo">¡Muchas gracias por tu compra!</h2>
+                    <h3 className="tiketCliente">**{(buyer.Nombre).toUpperCase()}**</h3>
+                    <p className="compra">La compra se ha realizado exitosamente</p>
+                    <p className="compra">Te enviamos un mail a <span className="mail">{(buyer.Email)}</span></p>
+                    <p className="compra">Tu nro. de orden es:</p>
+                    <p className="TiketId">{orderId}</p>
+                    <h2 className="tiketTituloBar">Rodolfo's Beer&Burger</h2>
+                    <Link to={'/'}><p className="carta">Realizar otra compra</p></Link>
+                </div>
+            </div>                        
+        )
+    }
+
+    return (
+    <>
+        <h1 className="FinalizandoCompra">Finalizando Compra</h1>
+        {
+        loading ? 
+        <Spinner /> : ( 
+        !orderId&&
+        <div className="formContenedorPrincipal">
+            <div className="formContenedor">
+                <form  onSubmit={ handleSubmit }  className="form">
+                    <h2 className='formText'>Completar Datos:</h2>
+                    <input 
+                        type="text"
+                        name="Nombre"
+                        placeholder=' Nombre Completo'
+                        value={Nombre}
+                        required
+                        onChange={handleInputChange} 
+                    />
+                     <input 
+                        type="text"
+                        name="Domicilio"
+                        placeholder=' Domicilio'
+                        value={Domicilio}
+                        required
+                        onChange={handleInputChange} 
+                    />
+                    <input
+                        type="text"
+                        name="Localidad"
+                        placeholder=' Localidad'
+                        value={Localidad}
+                        required
+                        onChange={handleInputChange} 
+                    />
+                    <input
+                        min="0"
+                        type="tel"
+                        name="Telefono"
+                        placeholder=' Telefono'
+                        value={Telefono}
+                        required
+                        onChange={handleInputChange} 
+                    />
+                    <input
+                        type="email"
+                        name="Email"
+                        placeholder=' mail@ejemplo.com'
+                        value={Email}
+                        required
+                        onChange={handleInputChange}
+                    />
+                    <input 
+                        type="email"
+                        name="ConfirmarEmail"                            
+                        placeholder=" Confirme su email "
+                        value={ConfirmarEmail}
+                        required
+                        onChange={handleInputChange} 
+                    />
+                    {
+                        buyer.Nombre && buyer.Domicilio && buyer.Telefono 
+                        && (buyer.Email === buyer.ConfirmarEmail) 
+                        && telefonoRegex.test(buyer.Telefono) 
+                        && emailRegex.test(buyer.Email, buyer.ConfirmarEmail)
+                    ? (
+                        // Botón habilitado
+                        <input 
+                            className="finalizarCompraActive"
+                            type="submit"
+                            value="Finalizar Compra"
+                        />
+                    ) : (
+                        // Botón deshabilitado
+                        <input 
+                        className="finalizarCompraDisable"
+                        type="subbmit"
+                        value="Finalizar Compra"
+                        disabled
+                    />
+                    )
+                    }
+                </form>
+           </div>
+        </div>
+        )}
+    </>    
+    )                        
+}
+export default Form;
